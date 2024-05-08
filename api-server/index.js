7c266aea9f5b379b4305e88a9a6ef7217d66d034
Demarquies Jolley <ahmir1128@gmail.com>
2024-05-08 18:45:28 -0500

    update

--- api-server/index.js
+++ api-server/index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const { db } = require('./src/models');
-const server = require('./src/server.js');
+const {server} = require('./src/server.js');
 const PORT = process.env.PORT || 3001
 
 db.sync().then(() => {
