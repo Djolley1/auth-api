a5b2548a636cf12d6a654957a79266c6ecbdc95f
Demarquies Jolley <ahmir1128@gmail.com>
2024-06-08 17:14:14 -0500

    moving folders

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-const { db } = require('./api-server/src/models');
-const app = require('./api-server/src/server.js');
+const { db } = require('./src/models');
+const app = require('./src/server.js');
 const PORT = process.env.PORT || 3001
 
 db.sync().then(() => {
